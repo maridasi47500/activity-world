1f452ec50d8ba7b6b095ddf33fc7eb027fc474c2
PIRATA DEL BARCO ANONYMOUS <thecat@thedog.themouse>
2025-08-09 23:44:31 +0200

    qsfg

--- src/clojurenew/core.clj
+++ src/clojurenew/core.clj
@@ -122,31 +122,31 @@
 (defn output
   "execute query and return lazy sequence"
   []
-  (query db ["select * from booking"]))
+  (query db ["select * from news"]))
 (defn getbyid
   "execute query and return lazy sequence"
   [myid]
-  (query db ["select * from booking where id = ?" myid]))
+  (query db ["select * from news where id = ?" myid]))
 
-(defn voirbain [title template req myid]
-  (println "action voirbain")
+(defn voirnews [title template req myid]
+  (println "action voirnew")
   (println req)
   (def mytemplate (slurp (io/resource template)))
   (def figure (map #(format mytemplate (get % :title "") (get % :body "") (get % :id "") (get % :id "")) (getbyid myid)))
-  (def body (format (slurp (io/resource "bains.html")) (str/join "" figure)))
+  (def body (format (slurp (io/resource "news.html")) (str/join "" figure)))
   (def title "hey")
-  (def hey (if (zero? (count (output))) "<p>il n'y a pas de bain à afficher</p>" body))
+  (def hey (if (zero? (count (output))) "<p>il n'y a pas de news à afficher</p>" body))
   (def reponsebody (format (slurp (io/resource "index.html")) title hey))
   (secondringhandler req reponsebody))
-(defn editbain [title template req myid]
-  (println "action voirbain")
+(defn editnews [title template req myid]
+  (println "action voirnews")
   (def mytemplate (slurp (io/resource template)))
   (println "action EDIT bain")
   (println (getbyid myid))
   (def figure (map #(format mytemplate (get % :id "") (get % :title "") (get % :body "") (get % :url "") (get % :id "")) (getbyid myid)))
-  (def body (format (slurp (io/resource "bains.html")) (str/join "" figure)))
+  (def body (format (slurp (io/resource "news.html")) (str/join "" figure)))
   (def title "hey")
-  (def hey (if (zero? (count (output))) "<p>il n'y a pas de bain à afficher</p>" body))
+  (def hey (if (zero? (count (output))) "<p>il n'y a pas de news à afficher</p>" body))
   (def reponsebody (format (slurp (io/resource "index.html")) title hey))
   (secondringhandler req reponsebody))
 
@@ -161,7 +161,7 @@
   (def reponsebody (format (slurp (io/resource "index.html")) title hey))
   (secondringhandler req reponsebody))
 
-(defn actiondelete [title req]
+(defn actiondeletenews [title req]
   (println "action delete")
   (def heyreader (io/reader (:body req) :encoding "UTF-8"))
   (println heyreader)
@@ -175,16 +175,16 @@
   (println myhash)
   (def id (get myhash :myid ""))
   (execute! db
-  ["delete from booking where id = ?"
+  ["delete from news where id = ?"
     id])
   (println "OHOHOH")
   ;(handlertworedirect req "/voir_reservations"))
   {:status 301
     :body (slurp (io/resource "redirect.html"))
     :contenttype "text/html"
-    :redirect "/voir_reservations"
+    :redirect "/voir_news"
     })
-(defn actionupdate [title hey req]
+(defn actionupdatenews [title hey req]
   (println "action create")
   (def heyreader (io/reader (:body req) :encoding "UTF-8"))
   (println heyreader)
@@ -199,16 +199,16 @@
   (def body (str/join "" (get myhash :body ["ur" "body"]) ))
   (def id (get myhash :id ""))
   (execute! db
-  ["update booking set title = ?, url = ?, body = ? where id = ?"
+  ["update news set title = ?, image = ?, body = ? where id = ?"
    mytitle url body id])
   (println "OHOHOH")
   {:status 301
     :body (slurp (io/resource "redirect.html"))
     :contenttype "text/html"
-    :redirect "/voir_bains"
+    :redirect "/voir_news"
     })
 
-(defn actioncreate [title hey req]
+(defn actioncreatenews [title hey req]
   (println "action create")
   (def heyreader (io/reader (:body req) :encoding "UTF-8"))
   (println heyreader)
@@ -220,34 +220,26 @@
   (def myhash (keywordize-keys (form-decode a)))
   (println "hey HEY he")
   (println myhash)
-  (def mytestdata {:nom (get myhash :nom "hye")
-                   :prenom (get myhash :prenom "url")
-                   :date (get myhash :date "url")
-                   :email (get myhash :email "url")
-                   :numero (get myhash :numero "url")
-                   :content (get myhash :content "url")
-                   :journee (get myhash :journee "url") })
-  (insert! db :booking mytestdata)
+  (def mytestdata {:image (get myhash :nom "hye")
+                   :title (get myhash :prenom "url")
+                   :content (get myhash :journee "url") })
+  (insert! db :news mytestdata)
   (println "OHOHOH")
   {:status 301
     :body (slurp (io/resource "redirect.html"))
     :contenttype "text/html"
-    :redirect "/voir_reservations"
+    :redirect "/voir_news"
     })
 
 (defn create-db
   "create db and table"
   []
   (try (db-do-commands db
-                       (create-table-ddl :booking
+                       (create-table-ddl :news
                                          [[:id "integer primary key autoincrement" ]
                                           [:timestamp :datetime :default :current_timestamp ]
-                                          [:prenom :text]
-                                          [:nom :text]
-                                          [:date :date]
-                                          [:numero :text]
-                                          [:email :text]
-                                          [:journee :text]
+                                          [:title :text]
+                                          [:image :text]
                                           [:content :text]]))
        (catch Exception e
          (println (.getMessage e)))))
@@ -262,7 +254,7 @@
 (defn output
   "execute query and return lazy sequence"
   []
-  (query db ["select * from booking"]))
+  (query db ["select * from news"]))
 
 
 
@@ -280,8 +272,8 @@
           (re-find #"^/$" uri) "welcome.html"
           (re-find #"^/$" uri) "welcome.html"
           (re-find #"^/hello$" uri) "hello.html"
-          (re-find #"^/create_booking$" uri) "uhiuh"
-          (re-find #"^/action_create_booking$" uri) "lk"
+          (re-find #"^/create_news$" uri) "uhiuh"
+          (re-find #"^/action_create_news$" uri) "lk"
           (re-find #"^/voir_bains$" uri) "lilu"
           (re-find #"^/voir_bain/\d+$" uri) "i"
           (re-find #"^/edit_bain/\d+$" uri) "i"
@@ -298,26 +290,26 @@
           (re-find #"^/app.js$" uri) (rendercss1 "Show my activity world" "app.js")
           (re-find #"^/hello$" uri) (renderhtml "Show my activity world" "hello.html")
           (re-find #"^/poster_news$" uri) (renderhtml "poster des news" "form.html")
-          (re-find #"^/action_create_news$" uri) (actioncreate "hello" "form.html" req)
-          (re-find #"^/action_update_news$" uri) (actionupdate "hello" "formedit.html" req)
-          (re-find #"^/deletenews" uri) (actiondelete "hello"  req)
+          (re-find #"^/action_create_news$" uri) (actioncreatenews "hello" "form.html" req)
+          (re-find #"^/action_update_news$" uri) (actionupdatenews "hello" "formedit.html" req)
+          (re-find #"^/deletenews" uri) (actiondeletenews "hello"  req)
           (re-find #"^/voir_news$" uri) (rendercollection "hello" output "_reservation.html" req)
           (re-find #"^/voir_news/\d+$" uri) (voirnews "hello" "voirnews.html" req (get (re-find #"^/voir_news/(\d+)$" uri) 1))
           (re-find #"^/edit_news/\d+$" uri) (editnews "hello" "formedit.html" req (get (re-find #"^/edit_news/(\d+)$" uri) 1))
-          (re-find #"^/poster_video$" uri) (renderhtml "poster des video" "form.html")
-          (re-find #"^/action_create_video$" uri) (actioncreate "hello" "form.html" req)
-          (re-find #"^/action_update_video$" uri) (actionupdate "hello" "formedit.html" req)
-          (re-find #"^/deletevideo" uri) (actiondelete "hello"  req)
-          (re-find #"^/voir_video$" uri) (rendercollection "hello" output "_reservation.html" req)
-          (re-find #"^/voir_video/\d+$" uri) (voirvideo "hello" "voirvideo.html" req (get (re-find #"^/voir_video/(\d+)$" uri) 1))
-          (re-find #"^/edit_video/\d+$" uri) (editvideo "hello" "formedit.html" req (get (re-find #"^/edit_video/(\d+)$" uri) 1))
-          (re-find #"^/poster_photo$" uri) (renderhtml "poster des photo" "form.html")
-          (re-find #"^/action_create_photo$" uri) (actioncreate "hello" "form.html" req)
-          (re-find #"^/action_update_photo$" uri) (actionupdate "hello" "formedit.html" req)
-          (re-find #"^/deletephoto" uri) (actiondelete "hello"  req)
-          (re-find #"^/voir_photo$" uri) (rendercollection "hello" output "_reservation.html" req)
-          (re-find #"^/voir_photo/\d+$" uri) (voirphoto "hello" "voirphoto.html" req (get (re-find #"^/voir_photo/(\d+)$" uri) 1))
-          (re-find #"^/edit_photo/\d+$" uri) (editphoto "hello" "formedit.html" req (get (re-find #"^/edit_photo/(\d+)$" uri) 1))
+          ;(re-find #"^/poster_video$" uri) (renderhtml "poster des video" "form.html")
+          ;(re-find #"^/action_create_video$" uri) (actioncreate "hello" "form.html" req)
+          ;(re-find #"^/action_update_video$" uri) (actionupdate "hello" "formedit.html" req)
+          ;(re-find #"^/deletevideo" uri) (actiondelete "hello"  req)
+          ;(re-find #"^/voir_video$" uri) (rendercollection "hello" output "_reservation.html" req)
+          ;(re-find #"^/voir_video/\d+$" uri) (voirvideo "hello" "voirvideo.html" req (get (re-find #"^/voir_video/(\d+)$" uri) 1))
+          ;(re-find #"^/edit_video/\d+$" uri) (editvideo "hello" "formedit.html" req (get (re-find #"^/edit_video/(\d+)$" uri) 1))
+          ;(re-find #"^/poster_photo$" uri) (renderhtml "poster des photo" "form.html")
+          ;(re-find #"^/action_create_photo$" uri) (actioncreate "hello" "form.html" req)
+          ;(re-find #"^/action_update_photo$" uri) (actionupdate "hello" "formedit.html" req)
+          ;(re-find #"^/deletephoto" uri) (actiondelete "hello"  req)
+          ;(re-find #"^/voir_photo$" uri) (rendercollection "hello" output "_reservation.html" req)
+          ;(re-find #"^/voir_photo/\d+$" uri) (voirphoto "hello" "voirphoto.html" req (get (re-find #"^/voir_photo/(\d+)$" uri) 1))
+          ;(re-find #"^/edit_photo/\d+$" uri) (editphoto "hello" "formedit.html" req (get (re-find #"^/edit_photo/(\d+)$" uri) 1))
           :else (renderhtml "Erreur" "404.html"))))
   (def status (get html :status 200))
   (def content (get html :contenttype "text/html"))
