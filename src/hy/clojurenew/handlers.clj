3aecfd42cac94e781959156fdb4dd44a4e310c8f
PIRATA DEL BARCO ANONYMOUS <thecat@thedog.themouse>
2025-08-13 22:23:51 +0200

    qg

---
+++ src/hy/clojurenew/handlers.clj
@@ -0,0 +1,93 @@
+(ns clojurenew.handlers
+  "Ring handlers for Activity World."
+  (:require [clojurenew.db :as db]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.walk :refer [keywordize-keys]]
+            [ring.util.response :as response]
+            [ring.util.codec :as codec]))
+
+(defn render-html
+  "Render an HTML template from resources."
+  [template & [params]]
+  (let [tpl (slurp (io/resource template))]
+    (if params
+      (apply format tpl params)
+      tpl)))
+
+(defn render-collection
+  "Render a collection of news into a template."
+  [title coll template]
+  (let [body (if (seq coll)
+               (str/join "" (map #(format template (:id %) (:title %) (:content %) (:url %) (:id %)) coll))
+               "<p>Il n'y a rien à afficher.</p>")
+        page (slurp (io/resource "index.html"))]
+    (format page title body)))
+
+(defn render-css [title file]
+  (-> (response/response (slurp (io/resource file)))
+      (response/content-type "text/css")))
+
+(defn render-js [title file]
+  (-> (response/response (slurp (io/resource file)))
+      (response/content-type "text/javascript")))
+
+(defn home [_]
+  (response/content-type
+    (response/response (render-html "hello.html"))
+    "text/html"))
+
+(defn poster-news [_]
+  (response/content-type
+    (response/response (render-html "form.html"))
+    "text/html"))
+
+(defn voir-news [_]
+  (response/content-type
+    (response/response
+      (render-collection
+        "Actualités"
+        (db/get-news)
+        (slurp (io/resource "_reservation.html"))))
+    "text/html"))
+
+(defn voir-news-id [req]
+  (let [id (get-in req [:params :id])
+        news (db/get-news-by-id id)]
+    (if news
+      (response/content-type
+        (response/response (render-html "voirnews.html" (:title news) (:content news) (:url news)))
+        "text/html")
+      (response/not-found "News not found"))))
+
+(defn edit-news-id [req]
+  (let [id (get-in req [:params :id])
+        news (db/get-news-by-id id)]
+    (if news
+      (response/content-type
+        (response/response (render-html "formedit.html" (:title news) (:content news) (:url news)))
+        "text/html")
+      (response/not-found "News not found"))))
+
+(defn action-create-news [req]
+  (let [params (if (:form-params req) (:form-params req) (:params req))]
+    (db/insert-news! params)
+    (-> (response/redirect "/voir_news")
+        (response/status 303))))
+
+(defn action-update-news [req]
+  (let [params (if (:form-params req) (:form-params req) (:params req))]
+    (db/update-news! params)
+    (-> (response/redirect "/voir_news")
+        (response/status 303))))
+
+(defn action-delete-news [req]
+  (let [id (get-in req [:params :id])]
+    (db/delete-news! id)
+    (-> (response/redirect "/voir_news")
+        (response/status 303))))
+
+(defn not-found-handler [_]
+  (-> (response/response (render-html "404.html"))
+      (response/status 404)
+      (response/content-type "text/html")))
