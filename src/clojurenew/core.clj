32a373eb8c976ad16f601e9f5c7436375f93c5c5
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2025-08-17 00:18:19 +0200

    fghjk

--- src/clojurenew/core.clj
+++ src/clojurenew/core.clj
@@ -11,12 +11,19 @@
             [ring.middleware.session :refer [wrap-session]]
             [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
             [ring.middleware.session.cookie :refer [cookie-store]]
-            [hiccup.core :refer [html]]))
+            [hiccup.core :refer [html]])
+  (import 'java.security.SecureRandom))
+
+(defn generate-key []
+  (let [bytes (byte-array 16)]
+    (.nextBytes (SecureRandom.) bytes)
+    bytes))
+
 
 (def app
   (-> app-routes
       (wrap-defaults (-> site-defaults
-                         (assoc-in [:session :store] (cookie-store {:key "abcdefg123456789"})))) ;a 16 long character string
+                         (assoc-in [:session :store] (cookie-store {:key generate-key})))) ;a 16 long character string
       (wrap-anti-forgery app-protected-routes)
       (wrap-session)
       (wrap-multipart-params {:store (byte-array-store)})))
